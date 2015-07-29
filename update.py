18c16ab542ba9ac1ab9143c1a23aca19ccc4772c
Alex Garcia <alex.garcia@yogoko.fr>
2015-07-29 09:58:25 +0200

    Add comments and template.html

--- update.py
+++ update.py
@@ -12,18 +12,23 @@ BASEDIR="site/"
 
 now = datetime.date.today()
 
+#get article basename
 articlename = sys.argv[1][0:sys.argv[1].find(".")]
 
+#read template & markdown content
 template = open("template.html", "r").read()
 md = codecs.open(sys.argv[1], mode="r", encoding="utf-8").read()
 
+#replace tags by what it needs to be
 template = template.replace("#TITLE#", sys.argv[2])
 template = template.replace("#CONTENT#", markdown.markdown(md, output_format="html5"))
 
 path = BASEDIR+"/"+str(now.year)+"/"+str(now.month)+"/"
 
+#Make the path if not exists
 os.makedirs(path, exist_ok=True)
 
+#Write the content
 output = codecs.open(path+articlename+".html", "w", encoding="utf-8", errors="xmlcharrefreplace")
 
 output.write(template)
